6ec1ae32c0affca8ee8275b32038f91003f6a1b1
Alexey <alexey.k@renova.st>
2021-09-05 17:55:10 +0600

    cli-plugin / remove html tag, fix font link

--- libs/vue-cli-plugin-nast/generator/template/app/layouts/App.vue
+++ libs/vue-cli-plugin-nast/generator/template/app/layouts/App.vue
@@ -20,5 +20,5 @@ export default {
 </script>
 
 <style lang="scss">
-
+@import url('https://fonts.googleapis.com/css?family=Raleway|Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i');
 </style>
