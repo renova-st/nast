761920d63d977a6cd9169092ca1ac5d0d9ebcb25
Alexey <alexey.k@renova.st>
2021-09-23 14:44:45 +0600

    fix bug with plugin install

--- libs/nast-ui/index.js
+++ libs/nast-ui/index.js
@@ -2,7 +2,7 @@ import NastLib from 'nast/Lib'
 // import parallax from './directives/parallax'
 // import mouseParallax from './directives/mouse-parallax'
 // import onVisible from './directives/on-visible'
-import clickOutside from './directives/click-outside'
+// import clickOutside from './directives/click-outside'
 import ui from '_ui'
 
 /**
@@ -23,15 +23,6 @@ export default class NastUi extends NastLib {
    * @return {Object}
    */
   static vue() {
-    return {
-      install(Vue, config) {
-        Vue.directive('click-outside', clickOutside)
-        // Vue.directive('mouse-parallax', mouseParallax)
-        // Vue.directive('on-visible', onVisible)
-        // Vue.directive('parallax', parallax)
-
-        ui(Vue)
-      },
-    }
+    return ui
   }
 }
